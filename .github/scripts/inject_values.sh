15a7db724df938f449d66250feaaaa79a66b9084
Christos Galanopoulos <xrholy11@gmail.com>
2023-01-24 16:54:01 +0200

    Update content

--- .github/scripts/inject_values.sh
+++ .github/scripts/inject_values.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ../..
+
 sed -i "s/giscus_repo_id_placeholder/$1/" _config.yml
 sed -i "s/giscus_category_id_placeholder/$2/" _config.yml
 sed -i "s/google_gtag_tracking_id_placeholder/$3/" _config.yml
